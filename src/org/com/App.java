577bb3ea72dc96ec29f633918d49be7c380ea45c
Suresh Pushpagiri <lakshman.tcs.123@gmail.com>
2018-12-23 22:03:42 +0530

    Just refinements

--- src/org/com/App.java
+++ src/org/com/App.java
@@ -3,5 +3,6 @@ package org.com;
 public class App {
     public static void main(String[] args) {
         System.out.println("testing app");
+        System.out.println("Second print stmt");
     }
 }
